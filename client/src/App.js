4111210a8c52c85e9355dc0056edb22e75bbf893
mcig <cig-mustafa.2001@hotmail.com>
2021-01-08 18:02:42 +0300

    added theme

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,44 @@ import Orders from "./routes/Orders";
 import Panel from "./routes/Panel";
 import Home from "./routes/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import MiniDrawer from "./components/MaterialUiBased/MiniDrawer";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const NetflixLightTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: "#e50914",
+    },
+  },
+  fontFamily: "Roboto",
+});
 
 export default function App() {
   const [links] = useState(["/", "/customers", "/orders", "/panel"]);
   return (
     <Router>
       <div>
-        <MiniDrawer links={links}>
-          <Switch>
-            <Route path="/customers">
-              <Customers />
-            </Route>
-            <Route path="/orders">
-              <Orders />
-            </Route>
-            <Route path="/panel">
-              <Panel />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </MiniDrawer>
+        <ThemeProvider theme={NetflixLightTheme}>
+          <MiniDrawer links={links}>
+            <Switch>
+              <Route path="/customers">
+                <Customers />
+              </Route>
+              <Route path="/orders">
+                <Orders />
+              </Route>
+              <Route path="/panel">
+                <Panel />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </MiniDrawer>
+        </ThemeProvider>
       </div>
     </Router>
   );
